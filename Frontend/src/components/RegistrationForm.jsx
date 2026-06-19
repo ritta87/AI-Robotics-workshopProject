7060f0899fa1ef9daf1820b0942d301a50429ab3
Ritta Ramachandran <rittaram87@gmail.com>
2026-06-19 15:11:56 +0530

    configure frontend to use deployed backend API

--- Frontend/src/components/RegistrationForm.jsx
+++ Frontend/src/components/RegistrationForm.jsx
@@ -21,7 +21,7 @@ function RegistrationForm() {
 
     try {
       const response = await fetch(
-        "http://localhost:5000/api/enquiry",
+         `${import.meta.env.VITE_API_URL}/api/enquiry` ,
         {
           method: "POST",
           headers: {
